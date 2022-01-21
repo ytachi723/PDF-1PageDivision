3832809bcf589edc8ab2fb538b57e165fc73c30e
Tachi <ytachi723@gmail.com>
2022-01-21 18:44:17 +0900

    All 20220121

---
+++ PDF1page分割/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace PDF1page分割
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("HGP創英角ｺﾞｼｯｸUB", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label.Location = new System.Drawing.Point(12, 9);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(771, 27);
+            this.label.TabIndex = 0;
+            this.label.Text = "フォルダー内のPDFファイルに対して、ページ毎にファイル分割します。";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "区切り文字：";
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox.Location = new System.Drawing.Point(169, 49);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(100, 34);
+            this.textBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(275, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(304, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ファイル名とページ番号の間の区切り文字です。";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(275, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(270, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "未入力のまま実行すると、「-」になります。";
+            // 
+            // button
+            // 
+            this.button.Font = new System.Drawing.Font("HGP創英角ﾎﾟｯﾌﾟ体", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button.Location = new System.Drawing.Point(343, 86);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 36);
+            this.button.TabIndex = 5;
+            this.button.Text = "開始";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 137);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label);
+            this.Name = "Form";
+            this.Text = "フォルダー内のPDFファイルをページ単位でファイルに！";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ PDF1page分割/Form1.cs
@@ -0,0 +1,65 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+namespace PDF1page分割
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            // 区切り文字設定
+            var delimiter = "-";
+            if (textBox.TextLength > 0)
+            {
+                delimiter = textBox.Text;
+            }
+
+            // フォルダー内のPDFファイルリスト取得
+            System.IO.DirectoryInfo nowFolder = new System.IO.DirectoryInfo(@".");
+            System.IO.SearchOption searchOption = System.IO.SearchOption.TopDirectoryOnly;
+            IEnumerable<System.IO.FileInfo> now = nowFolder.EnumerateFiles("*.pdf", searchOption);
+            PdfDocumentOpenMode pdfOpenMode = PdfDocumentOpenMode.Import;
+            PdfDocument targetPDF;
+
+            // フォルダー内のPDFをリストから１つずつ実施
+            foreach (var item in now)
+            {
+                try
+                {
+                    targetPDF = PdfReader.Open(item.DirectoryName + "\\" + item.ToString(), pdfOpenMode);
+                    var page = 1;
+
+                    // １ページずつ保存
+                    foreach (PdfPage PDFpage in targetPDF.Pages)
+                    {
+                        // PDF頁を追加
+                        PdfDocument outputPDF = new PdfDocument();
+                        outputPDF.AddPage(PDFpage);
+                        outputPDF.Save(now.First().DirectoryName + "\\" + Path.GetFileNameWithoutExtension(item.ToString()) + delimiter + page.ToString() + ".pdf");
+                        outputPDF.Close();
+                        page++;
+                    }
+                    targetPDF.Close();
+                }
+                catch(PdfSharp.Pdf.IO.PdfReaderException)
+                {
+                    continue;
+                }
+            }
+            Application.Exit();
+        }
+    }
+}
